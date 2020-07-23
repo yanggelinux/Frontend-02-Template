e57f6072fc05751723e0e4d15cc662086ab49982
yangyang <yangyang@ishansong.com>
2020-07-23 10:18:28 +0800

    fix

--- week04/homework07.js
+++ week04/homework07.js
@@ -65,13 +65,69 @@ class Request {
   }
 }
 class ResponsePaeser {
-  constructor() {}
+  constructor() {
+    this.WAITING_STATUS_LINE = 0
+    this.WAITING_STATUS_LINE_END = 1
+    this.WAITING_HEADER_NAME = 2
+    this.WAITING_HEADER_SPACE = 3
+    this.WAITING_HEADER_VALUE = 4
+    this.WAITING_HEADER_LINE_END = 5
+    this.WAITING_HEADER_BLOCK_END = 6
+    this.WAITING_BODY = 6
+
+    this.current = this.WAITING_STATUS_LINE
+    this.statusLine = ""
+    this.headers = {}
+    this.headerName = ""
+    this.headerValue = ""
+    this.bodyParser = null
+  }
   receive(string) {
     for (let i = 0; i < string.length; i++) {
       this.receiveChar(string.charAt(i))
     }
   }
-  receiveChar(char) {}
+  receiveChar(char) {
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === "\r") {
+        this.current = this.WAITING_STATUS_LINE_END
+      } else {
+        this.statusLine += char
+      }
+    } else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === "\n") {
+        this.current = this.WAITING_HEADER_NAME
+      }
+    } else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ":") {
+        this.current = this.WAITING_HEADER_SPACE
+      } else if (char === "\r") {
+        this.current = this.WAITING_HEADER_BLOCK_END
+      } else {
+        this.headerName += char
+      }
+    } else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === " ") {
+        this.current = this.WAITING_HEADER_VALUE
+      }
+    } else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === "\r") {
+        this.current = this.WAITING_HEADER_LINE_END
+        this.headers[this.headerName] = this.headerValue
+        this.headerName = ""
+        this.headerValue = ""
+      } else {
+        this.headerValue += char
+      }
+    } else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === "\n") {
+        this.current = this.WAITING_BODY
+      }
+    } else if (this.current === this.WAITING_BODY) {
+      console.log(char)
+      console.log(this.statusLine)
+    }
+  }
 }
 
 void (async function() {
