22e71e1a4bf7a49f476234162300b0f1f072bae5
yangyang <yangyang@ishansong.com>
2020-07-29 20:30:52 +0800

    homework 05

---
+++ week05/server.js
@@ -0,0 +1,27 @@
+// 用 Node.js 写一个小的服务端
+
+const http = require("http")
+
+http
+  .createServer((request, response) => {
+    let body = []
+    request
+      .on("error", (err) => {
+        console.log(err)
+      })
+      .on("data", (chunk) => {
+        console.log("chunk.toString()", chunk.toString())
+        body.push(chunk.toString())
+      })
+      .on("end", () => {
+        // body = Buffer.concat(body).toString()
+        body = body.join("")
+        console.log("body", body)
+        response.writeHead(200, { "Content-Tyoe": "text/html" })
+        response.end(
+          `<html lang="cn"><head><style>.class{color:read}</style></head><body><div class="container"><p>hahahha</p></div></body></html>`
+        )
+      })
+  })
+  .listen(8088)
+console.log("server started")
