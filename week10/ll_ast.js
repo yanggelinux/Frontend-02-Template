95c5fafa57389c3c5be7e3dda620071593911291
yangyang <yangyang@ishansong.com>
2020-08-31 19:59:39 +0800

    fix

---
+++ week10/ll_ast.js
@@ -0,0 +1,45 @@
+const regexp = /([0-9\.]+)|([ \t]+)|([\r\n]+)|(\*)|(\/)|(\+)|(\-)/g
+
+const dictionary = [
+  "Number",
+  "Whitespace",
+  "LineTerminator",
+  "*",
+  "/",
+  "+",
+  "-",
+]
+
+function* tokenize(source) {
+  let result = null
+  let lastIndex = 0
+  while (true) {
+    console.log("regexp.lastIndex", regexp.lastIndex)
+    lastIndex = regexp.lastIndex
+    result = regexp.exec(source)
+    if (!result) {
+      break
+    }
+    if (regexp.lastIndex - lastIndex > result[0].length) {
+      break
+    }
+    let token = {
+      type: null,
+      value: null,
+    }
+    for (let i = 1; i < dictionary.length; i++) {
+      if (result[i]) {
+        token.type = dictionary[i - 1]
+      }
+    }
+    token.value = result[0]
+    yield token
+  }
+  yield {
+    type: "EOF",
+  }
+}
+
+for (let token of tokenize("1024 +10 * 25")) {
+  console.log(token)
+}
