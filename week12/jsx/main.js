8b17deeb7abc74f0490973cd3f1b4e3a2c8d98c2
yangyang <yangyang@ishansong.com>
2020-09-17 20:26:57 +0800

    add homework

--- week12/jsx/main.js
+++ week12/jsx/main.js
@@ -16,15 +16,60 @@ class Carousel extends Componet {
       child.style.backgroundImage = `url('${record}')`
       this.root.appendChild(child)
     }
-    let current = 0
-    setInterval(() => {
+
+    let position = 0
+    this.root.addEventListener("mousedown", (event) => {
       let children = this.root.children
-      ++current
-      current = current % children.length
-      for (let child of children) {
-        child.style.transform = `translateX(-${current * 100}%)`
+      let startX = event.clientX
+      let move = (event) => {
+        let x = event.clientX - startX
+        let current = position - (x - (x % 500)) / 500
+        for (let offset of [-1, 0, 1]) {
+          let pos = current + offset
+          pos = (pos + children.length) % children.length
+          children[pos].style.transition = "none"
+          children[pos].style.transform = `translateX(${-pos * 500 +
+            offset * 500 +
+            (x % 500)}px)`
+        }
       }
-    }, 1000)
+      let up = (event) => {
+        let x = event.clientX - startX
+        position = position - Math.round(x / 500)
+        for (let offset of [
+          0,
+          -Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x)),
+        ]) {
+          let pos = position + offset
+
+          pos = (pos + children.length) % children.length
+          children[pos].style.transition = ""
+          children[pos].style.transform = `translateX(${-pos * 500 +
+            offset * 500}px)`
+        }
+        document.removeEventListener("mousemove", move)
+        document.removeEventListener("mouseup", up)
+      }
+      document.addEventListener("mousemove", move)
+
+      document.addEventListener("mouseup", up)
+    })
+
+    // let currentIndex = 0
+    // setInterval(() => {
+    //   let children = this.root.children
+    //   let nextIndex = (currentIndex + 1) % children.length
+    //   let current = children[currentIndex]
+    //   let next = children[nextIndex]
+    //   next.style.transition = "none"
+    //   next.style.transform = `translateX(${100 - nextIndex * 100}%)`
+    //   setTimeout(() => {
+    //     next.style.transition = ""
+    //     current.style.transform = `translateX(${-100 - currentIndex * 100}%)`
+    //     next.style.transform = `translateX(${-nextIndex * 100}%)`
+    //     currentIndex = nextIndex
+    //   }, 16)
+    // }, 1000)
     return this.root
   }
   mountTo(parent) {
