d7fde1784518561d4f339c7e9b4bfb698f1e25fa
yangyang <yangyang@ishansong.com>
2020-10-20 20:23:07 +0800

    fix

---
+++ week16/toolchain/generators/app/index.js
@@ -0,0 +1,49 @@
+var Generator = require("yeoman-generator")
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    // Calling the super constructor is important so our generator is correctly set up
+    super(args, opts)
+
+    // Next, add your custom code
+    // this.option("babel") // This method adds support for a `--babel` flag
+  }
+  async method1() {
+    const answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname, // Default to current folder name
+      },
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?",
+      },
+    ])
+    this.log("app name", answers.name)
+    this.log("cool feature", answers.cool)
+  }
+  async step1() {
+    this.fs.copyTpl(
+      this.templatePath("index.html"),
+      this.destinationPath("public/index.html"),
+      { title: "Templating with Yeoman" }
+    )
+  }
+  initPackege() {
+    const pkgJson = {
+      devDependencies: {
+        eslint: "^3.15.0",
+      },
+      dependencies: {
+        react: "^16.2.0",
+      },
+    }
+
+    // Extend or create package.json file in destination path
+    this.fs.extendJSON(this.destinationPath("package.json"), pkgJson)
+    this.npmInstall()
+  }
+}
