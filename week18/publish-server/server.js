e88aea3745727d5b88cf30ceb3015ebe1c1556f2
yangyang <yangyang@ishansong.com>
2020-11-03 19:50:54 +0800

    add week18 homework

--- week18/publish-server/server.js
+++ week18/publish-server/server.js
@@ -1,12 +1,91 @@
 let http = require("http")
 let fs = require("fs")
 let unzipper = require("unzipper")
+let querystring = require("querystring")
+
+//2、auth路由：接受code，用code+clinet_id+client_secret换token
+function auth(request, response) {
+  let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+$)/)[1])
+  getToken(query.code, function (info) {
+    response.write(
+      `<a href="http://localhost:8083/?token=${info.access_token}">publish</a>`
+    )
+    response.end()
+  })
+}
+
+function getToken(code, callback) {
+  let request = http.request(
+    {
+      hostname: "github.com",
+      path: `/login/oauth/access_token?code=${code}?client_id=Iv1.73f6a40000cece08?client_secret=57e0a846c1605e2aa4d2ecab1c1730a25442ced6`,
+      port: 443,
+      method: "POST",
+    },
+    function (response) {
+      let body = ""
+      response.on("data", (chunk) => {
+        body += chunk.toString()
+      })
+      response.on("end", (chunk) => {
+        console.log(body)
+        callback(querystring.parse(body))
+      })
+    }
+  )
+  request.end()
+}
+
+function getUser(token, callback) {
+  let request = http.request(
+    {
+      hostname: "api.github.com",
+      path: `/user`,
+      port: 443,
+      method: "GET",
+      headers: {
+        Authorization: `token ${token}`,
+        "User-Agent": "yang-publish",
+      },
+    },
+    function (response) {
+      let body = ""
+      response.on("data", (chunk) => {
+        body += chunk.toString()
+      })
+      response.on("end", (chunk) => {
+        callback(JSON.parse(body))
+      })
+    }
+  )
+  request.end()
+}
+
+//4、publush路由：用token获取用户信息，检查权限，接受发布
+
+function publish(request, response) {
+  let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+$)/)[1])
+  getUser(query.token, (info) => {
+    if (info.login === "yanggelinux") {
+      request.pipe(unzipper.Extract({ path: "../server/public" }))
+      request.on("end", function () {
+        response.end("Success")
+      })
+    }
+  })
+}
 
 http
   .createServer(function (request, response) {
+    if (request.url.match(/^\/auth\?/)) {
+      return auth(request, response)
+    }
+    if (request.url.match(/^\/publish\?/)) {
+      return publish(request, response)
+    }
     // let outFile = fs.createWriteStream("../server/public/index.html")
     // request.pipe(outFile)
-    request.pipe(unzipper.Extract({ path: "../server/public" }))
+    //request.pipe(unzipper.Extract({ path: "../server/public" }))
     // request.on("data", (chunk) => {
     //   outFile.write(chunk)
     // })
--- week18/publish-tool/publish.js
+++ week18/publish-tool/publish.js
@@ -1,35 +1,52 @@
 let http = require("http")
 let fs = require("fs")
 let archiver = require("archiver")
+let child_process = require("child_process")
 
-let request = http.request(
-  {
-    hostname: "127.0.0.1",
-    port: 8082,
-    method: "POST",
-    headers: { "Content-Type": "application/octet-stream" },
-  },
-  (response) => {
-    console.log(response)
-  }
+//1、打开https://github.com/login/oauth/authorize
+child_process.exec(
+  `open https://github.com/login/oauth/authorize?client_id=Iv1.73f6a40000cece08`
 )
-// let file = fs.createReadStream("./sample.html")
+//3、创建server，接受token，点击发布
 
-let archive = archiver("zip", {
-  zlib: { level: 9 },
-})
-archive.directory("./sample/", false)
-archive.finalize()
-archive.pipe(request)
+http
+  .createServer(function (request, response) {
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+$)/)[1])
+    publish(query.token)
+  })
+  .listen(8083)
 
-// file.pipe(request)
-// file.on("end", () => request.end())
-// file.on("data", (chunk) => {
-//   console.log(chunk.toString())
-//   request.write(chunk)
-// })
+function publish(token) {
+  let request = http.request(
+    {
+      hostname: "127.0.0.1",
+      port: 8082,
+      method: "POST",
+      path: `publish?token=${token}`,
+      headers: { "Content-Type": "application/octet-stream" },
+    },
+    (response) => {
+      console.log(response)
+    }
+  )
+  // let file = fs.createReadStream("./sample.html")
 
-// file.on("end", (chunk) => {
-//   console.log("read finshed")
-//   request.end(chunk)
-// })
+  let archive = archiver("zip", {
+    zlib: { level: 9 },
+  })
+  archive.directory("./sample/", false)
+  archive.finalize()
+  archive.pipe(request)
+
+  // file.pipe(request)
+  // file.on("end", () => request.end())
+  // file.on("data", (chunk) => {
+  //   console.log(chunk.toString())
+  //   request.write(chunk)
+  // })
+
+  // file.on("end", (chunk) => {
+  //   console.log("read finshed")
+  //   request.end(chunk)
+  // })
+}
